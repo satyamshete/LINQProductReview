8a895d175b407240a0755ec4928c337ea2d07f41
satyamshete <shetesatyam99@gmail.com>
2022-10-02 17:25:55 +0530

    [Satyam] Add Product Details

---
+++ LINQoperations/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace LINQoperations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prodcut Review Management!!!!!!!!");
+            Console.WriteLine("Adding a Prodcut Review In list");
+            Console.WriteLine("Enter Option" +
+                              "\n1.Add Values to list" 
+                              );
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Creating a list for Product Review 
+            List<ProductReview> productReviews = new List<ProductReview>();
+            switch (option)
+            {
+                case 1:
+                    ProductReviewManager.AddingProductReview(productReviews);
+                    break;
+                default:
+                    Console.WriteLine("Enter proper selection");
+                    break;
+            }
+        }
+    }
+}
